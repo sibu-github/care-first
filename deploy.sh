97244385272e86bdf4f32231bcad287b07f4a42f
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-06 13:15:35 +0530

    deploying angular build

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "making angular build"
+
+echo "switching to frontend repository"
 cd /Users/sibaprasad/neutrinos-studio/health-app/app
+echo "switch to master branch"
 git checkout master
+echo "remove dist folder"
 rm -rf dist 
+echo "making angular build"
 gulp && ng build --configuration=prod --aot --build-optimizer && gulp build-web 
 echo "angular build finished"
+echo "switching to care-first repository"
 cd /Users/sibaprasad/works/BCD/care-first
+echo "remove dist folder"
+rm -rf dist 
 echo "copying dist folder"
 mv /Users/sibaprasad/neutrinos-studio/health-app/app/dist ./
 echo "commiting to git"
