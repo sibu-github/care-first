304c80bc7c88f085b04f0ab1312b09a5202c9eee
Sibaprasad Maiti <sibu.it13@gmail.com>
2020-06-09 14:17:41 +0530

    deploying angular build

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 echo "switching to frontend repository"
 cd /Users/sibaprasad/neutrinos-studio/health-app/app
 echo "switch to master branch"
-# git checkout master
-git checkout magic
+git checkout master
+# git checkout magic
 echo "remove dist folder"
 rm -rf dist 
 echo "making angular build"
